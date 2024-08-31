0ad7c34c0152d6291ff2189215d3560811e9bfb2
Jonas Olmstead <>
2024-08-31 21:29:56 +0200

    reduce code

--- src/js/ui/MiniMap.js
+++ src/js/ui/MiniMap.js
@@ -25,27 +25,14 @@ GLOBAL.miniMap = {
 			vec2(3),
 			tile(vec2(36, 144), vec2(36, 36))
 		);
-		for (let y = 0; y < GLOBAL.mapMan.mapHeight; y++) {
-			for (let x = 0; x < GLOBAL.mapMan.mapWidth; x++) {
-				const val = GLOBAL.mapGrid[y][x];
-				if (val) {
-					let color = new Color(75 / 255, 105 / 255, 47 / 255); // tree
-					let size = vec2(1 / 12);
-					if (val instanceof Stone) {
-						color = new Color(155 / 255, 173 / 255, 183 / 255)
-					}
 
-					if (typeof(val) !== 'string') {
-						drawRect(
-							uiPos.add(vec2((x - 18) / 12, (y - 18) / 12)),
-							size,
-							color
-						);
-					}
-				}
-			}
-		}
-		let color = new Color(153 / 255, 229 / 255, 80 / 255);
+		let color = new Color(75 / 255, 105 / 255, 47 / 255);
+		GLOBAL.miniMap.drawObjects(uiPos, GLOBAL.trees, color);
+
+		color = new Color(155 / 255, 173 / 255, 183 / 255);
+		GLOBAL.miniMap.drawObjects(uiPos, GLOBAL.stones, color);
+
+		color = new Color(153 / 255, 229 / 255, 80 / 255);
 		GLOBAL.miniMap.drawObjects(uiPos, GLOBAL.buildings, color);
 		GLOBAL.miniMap.drawObjects(uiPos, GLOBAL.units, color);
 
